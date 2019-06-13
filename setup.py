9951bd13aefa5a35b921131846ef1e74de6939a7
Wesley Kerfoot <wjak56@gmail.com>
2019-06-13 10:53:02 -0400

    Use semver and bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spotify-mpd-sync",
-    version="1.0",
+    version="1.0.1",
     author="Wesley Kerfoot",
     author_email="wes@wesk.tech",
     description="Synchronize Spotify Playlist to MPD",
